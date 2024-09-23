9e234aac20785c94489d52a90df69726b5710da6
sg-812 <soumi.webskitters@gmail.com>
2024-09-23 10:01:44 +0530

    changes in footer and routing

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -7,7 +7,7 @@ function FooterPart() {
       <Card className="text-center">
         <Card.Header>Featured</Card.Header>
         <Card.Body>
-          <Card.Title>Special title treatment</Card.Title>
+          <Card.Title>Footer Part</Card.Title>
           <Card.Text>
             With supporting text below as a natural lead-in to additional
             content.
--- src/routing/Routing.js
+++ src/routing/Routing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route,Routes,BrowserRouter } from 'react-router-dom'
 
 const Routing = () => {
   return (
